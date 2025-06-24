1e09ac57e075565ffbaf81b77b7e58837e63565c
Audrey Castillo <valax321@gmail.com>
2025-06-24 11:53:05 +1000

    Integrate Dynamic Cloud Sync
    Implements latest API changes for PlatformAPI

--- Runtime/SteamworksSaveData.cs
+++ Runtime/SteamworksSaveData.cs
@@ -8,7 +8,37 @@ namespace Radish.PlatformAPI.Steamworks
     internal sealed class SteamworksSaveData : IPlatformSaveData
     {
         public bool isSupported => SteamClient.IsValid;
-        
+
+        public event IPlatformSaveData.SaveDataDynamicChangeDelegate onSaveDataChanged;
+
+        public SteamworksSaveData()
+        {
+            SteamRemoteStorage.OnLocalFileChange += OnFilesChanged;
+        }
+
+        private void OnFilesChanged()
+        {
+            var changes = new List<IPlatformSaveData.FileChange>();
+            var changeCount = SteamRemoteStorage.GetLocalFileChangeCount();
+            for (var i = 0; i < changeCount; ++i)
+            {
+                var path = SteamRemoteStorage.GetLocalFileChange(i, out var change, out var type);
+                
+                // We don't use autocloud via this API, so skip those changes
+                if (type == RemoteStorageFilePathType.Absolute)
+                    continue;
+                
+                changes.Add(new IPlatformSaveData.FileChange(path, change switch
+                {
+                    RemoteStorageLocalFileChange.FileUpdated => IPlatformSaveData.ChangeType.Updated,
+                    RemoteStorageLocalFileChange.FileDeleted => IPlatformSaveData.ChangeType.Deleted,
+                    _ => throw new ArgumentOutOfRangeException()
+                }));
+            }
+            
+            onSaveDataChanged?.Invoke(changes);
+        }
+
         public bool BeginDataWrite()
         {
             return SteamRemoteStorage.BeginFileWriteBatch();
